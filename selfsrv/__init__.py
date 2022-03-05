312f9fdf88ed3050043d94b9f5ed4b0e2583e098
Benjamin Marasco <marascoben@gmail.com>
2022-03-05 01:48:06 -0500

    Update __init__.py

--- selfsrv/__init__.py
+++ selfsrv/__init__.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 def start():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/')
+
+     # Use a non random key as long as the environment is explicitly set to development
+    if os.getenv('FLASK_ENV') == 'development':
+        app.secret_key = b'SUPERSECRETKEY'
+        app.debug = True
+    else:
+        app.secret_key = os.urandom(24)
 
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return render_template('index.html')
 
     return app
 
 if __name__ == '__main__':
+    app = start()
     app.run(debug=True)
